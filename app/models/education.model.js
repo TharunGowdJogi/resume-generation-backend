f6e9b7698b29073346a49aa3288c71191a58ff4a
snathala26 <149908638+snathala26@users.noreply.github.com>
2024-07-15 23:49:15 -0400

    bug fix in education and employment models for  to_year field

--- app/models/education.model.js
+++ app/models/education.model.js
@@ -1,52 +1,64 @@
 module.exports = (sequelize, Sequelize) => {
-    const Education = sequelize.define("education", {
-      education_id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+  const Education = sequelize.define("education", {
+    education_id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    resume_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    from_year: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    to_year: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+    organization: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    course_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    gpa: {
+      type: Sequelize.DECIMAL(3, 2),
+      allowNull: true,
+    },
+    state: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    city: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    country: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    details: {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    },
+  }, {
+    hooks: {
+      beforeCreate: (education, options) => {
+        if (!education.to_year) {
+          education.to_year = null;
+        }
       },
-      resume_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      from_year: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      to_year: {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-      },
-      organization: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      course_name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      gpa: {
-        type: Sequelize.DECIMAL(3, 2),
-        allowNull: true,
-      },
-      state: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      city: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      country: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      details: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      },
+      beforeUpdate: (education, options) => {
+        if (!education.to_year) {
+          education.to_year = null;
+        }
+      }
+    }
     });
-  
-    return Education;
-  };
-  
+
+  return Education;
+};
